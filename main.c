42559292cf312ff76d22b51d68494b0c2b5db964
progra <progra@progra>
2025-05-21 14:39:51 -0400

    main pro

--- main.c
+++ main.c
@@ -6,11 +6,24 @@
  */
 
 #include <stdint.h>
+
 #define bitSet(puerto, bit) (1<<(bit) | (puerto))
 #define bitClr(puerto, bit) (0<<(bit) & (puerto))
 #define bitGet(puerto, bit) (1<<(bit) & (puerto)?1:0)
 #define bitToggle(puerto, bit) (1<<(bit) ^ (puerto))
+#define maskOn(puerto, mascara) ((mascara) | (puerto))
+#define maskOff(puerto, mascara) (~(mascara) & (puerto))
+#define maskToggle(puerto, mascara) ((mascara) ^ (puerto))
+
 /*
+bitSet: Dado un puerto y un número de bit, debe cambiar su estado a 1.
+
+bitClr: Dado un puerto y un número de bit, debe cambiar su estado a 0.
+
+bitGet: Dado un puerto y un número de bit, debe devolver su valor.
+
+bitToggle: Dado un puerto y un número de bit, debe cambiar al estado opuesto en el que está (si está en 0
+pasar a 1, y si está en 1 pasar a 0).
 
 maskOn: Dado un puerto y una máscara, debe prender todos aquellos bits que estén prendidos en la máscara,
 sin cambiar el estado de los restantes. Por ejemplo, dado el puerto A, que originalmente se encuentra en el
@@ -24,16 +37,22 @@ maskToggle: Dado un puerto y una máscara, debe cambiar el estado de todos aquel
 en la máscara al opuesto, sin cambiar el estado de los restantes. Por ejemplo, dado el puerto A, que
  originalmente se encuentra en el estado 0x02, al aplicarle la máscara 0x03, el resultado será 0x01.
 */
+
+typedef struct{
+			int8_t puertoA;
+		    int8_t puertoB;
+		    int16_t puertoD;
+	}puertos_t;
+
 int main(void){
-	int8_t puertoA;
-	int8_t puertoB;
-	int16_t puertoD;
-	puertoA = 2;
-	puertoB = 4;
-	printf("%d\n", bitSet(puertoA, 0));
-	printf("%d\n", bitClr(puertoB, 2));
-	printf("%d\n", bitGet(puertoA, 0));
-	printf("%d\n", bitToggle(puertoA, 1));
+	static puertos_t puertos = {2, 4};
+	printf("%d\n", bitSet(puertos.puertoA, 0));
+	printf("%d\n", bitClr(puertos.puertoB, 2));
+	printf("%d\n", bitGet(puertos.puertoA, 0));
+	printf("%d\n", bitToggle(puertos.puertoA, 0));
+	printf("%X\n", maskOn(0x01, 0x0A));
+	printf("%X\n", maskOff(0x0A, 0x02));
+	printf("%X\n", maskToggle(0x02, 0x03));
 }
 
 
